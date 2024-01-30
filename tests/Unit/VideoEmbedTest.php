5869c6a4fef39adabb79a151c97e227dd1eb8604
Tatiana <t.pshenyshna@amgrade.com>
2024-01-30 15:05:46 +0200

    VE-5697: Rename variables in VideoEmbedTest

--- tests/Unit/VideoEmbedTest.php
+++ tests/Unit/VideoEmbedTest.php
@@ -10,16 +10,21 @@ use PHPUnit\Framework\TestCase;
 
 class VideoEmbedTest extends TestCase
 {
+    public function setUp(): void
+    {
+        parent::setUp();
+    }
+
     /**
      * @throws \Illuminate\Contracts\Container\BindingResolutionException
      */
     public function testParsers(): void
     {
-        $videoIdParserData = require __DIR__ . '/../data/video-embed.php';
+        $videoEmbedData = require __DIR__ . '/../data/video-embed.php';
 
         $parser = Container::getInstance()->make(VideoParser::class);
 
-        foreach ($videoIdParserData as $key => $value) {
+        foreach ($videoEmbedData as $key => $value) {
             foreach ($value as $item) {
                 $this->assertEquals(
                     $item['expected'],
